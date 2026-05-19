5adf56b9daabeb5fd42f181d73a89986d31a1e41
pokochan <kohki.hidai@gmail.com>
2026-05-19 17:40:07 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,14 @@
-self.addEventListener('fetch', () => {
+self.addEventListener('install', (event) => {
+  console.log('SW: Install Event');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('SW: Activate Event');
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  // ネットワークにそのまま通す（空にせず、これを書くのが超重要！）
+  event.respondWith(fetch(event.request));
 });
